3b52a0aa68a1595b23e6c43bce2e26e3a04d61a2
nalhashimi <naamanh@gmail.com>
2024-12-03 15:59:27 -0500

    clean

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Events\ShortUrlCreated;
-use App\Listeners\SendShortUrlNotification;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
